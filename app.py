30e01aaf1f319b10f4f406cb24edee42f16e006d
Hunter John Larco <hjlarco@gmail.com>
2015-02-25 16:51:29 -0500

    Switch to app.py setup

---
+++ app.py
@@ -0,0 +1,23 @@
+from google.appengine.ext.webapp import template
+import webapp2
+import os
+
+
+class Landing(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'templates/landing.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+class Playground(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'templates/playground.html')
+    self.response.out.write(template.render(path, template_values))
+
+
+app = webapp2.WSGIApplication([
+  ('/playground/?', Playground),
+  ('/.*', Landing)
+], debug=True)
