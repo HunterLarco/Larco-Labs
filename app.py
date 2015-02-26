051dd4cf36aa59e90153462af546d130539f25a5
Hunter John Larco <hjlarco@gmail.com>
2015-02-25 21:14:08 -0500

    Contact Page

--- app.py
+++ app.py
@@ -17,7 +17,15 @@ class Playground(webapp2.RequestHandler):
     self.response.out.write(template.render(path, template_values))
 
 
+class Contact(webapp2.RequestHandler):
+  def get(self):
+    template_values = {}
+    path = os.path.join(os.path.dirname(__file__), 'templates/contact.html')
+    self.response.out.write(template.render(path, template_values))
+
+
 app = webapp2.WSGIApplication([
   ('/playground/?', Playground),
+  ('/contact/?', Contact),
   ('/.*', Landing)
 ], debug=True)
